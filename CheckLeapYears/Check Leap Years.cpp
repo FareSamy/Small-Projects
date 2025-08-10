068a23ad46a51982e83171b27926670dc140cc24
Fares <fares.samy1@gmail.com>
2025-08-11 00:57:07 +0300

    Add files via upload

---
+++ CheckLeapYears/Check Leap Years.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+bool IsLeapYear(short Year)
+{
+
+    if (Year % 400 == 0)
+    {
+        return true;
+    }
+    else if (Year % 100 ==0)
+    {
+        return false;
+    }
+    else if (Year % 4 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+short ReadYear()
+{
+    short Year;
+    cout << "Please Enter a year to check ?." << endl;
+    cin >> Year;
+    return Year;
+}
+int main()
+{
+    short Year = ReadYear();
+
+    if (IsLeapYear(Year))
+    
+        cout << "Yes , Year [" << Year << "] is Leap Year. " << endl;
+    else
+        cout << "No , Year [" << Year << "] is  Not Leap Year. " << endl;
+
+    system("pause>0");
+    return 0;
+
+
+}
